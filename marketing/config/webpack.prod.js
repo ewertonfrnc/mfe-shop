ebf810439efdc649788070167cfcf6fb261c8894
Ewerton <ewefsilva@gmail.com>
2024-08-20 19:53:01 -0300

    updated expose name on marketing

--- marketing/config/webpack.prod.js
+++ marketing/config/webpack.prod.js
@@ -1,7 +1,7 @@
 const { merge } = require("webpack-merge");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
-const commonConfig = require("./webpack.common");
 const packageJson = require("../package.json");
+const commonConfig = require("./webpack.common");
 
 const prodConfig = {
   mode: "production",
@@ -11,7 +11,7 @@ const prodConfig = {
   },
   plugins: [
     new ModuleFederationPlugin({
-      name: "container",
+      name: "marketing",
       filename: "remoteEntry.js",
       exposes: {
         "./MarketingApp": "./src/bootstrap",
