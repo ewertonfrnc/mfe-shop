6cf3ef6f8b767d407ce0ba79dbe272e78458981d
Ewerton <ewefsilva@gmail.com>
2024-08-21 21:21:04 -0300

    added navigation on container and marketing

--- container/src/components/marketing-app.js
+++ container/src/components/marketing-app.js
@@ -1,11 +1,26 @@
 import { mount } from "marketing/MarketingApp";
+
 import React, { useRef, useEffect } from "react";
+import { useHistory } from "react-router-dom";
 
 export default function MarketingApp() {
+  const history = useHistory();
   const ref = useRef(null);
 
   useEffect(() => {
-    mount(ref.current);
+    const { onParentNavigate } = mount(ref.current, {
+      onNavigate: ({ pathname: nextPathname }) => {
+        const { pathname } = history.location;
+
+        if (pathname !== nextPathname) {
+          history.push(nextPathname);
+        }
+      },
+    });
+
+    history.listen((location) => {
+      onParentNavigate(location);
+    });
   }, []);
 
   return <div ref={ref} />;
--- marketing/src/App.js
+++ marketing/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Switch, Route, BrowserRouter } from "react-router-dom";
+import { Switch, Route, Router } from "react-router-dom";
 import { StylesProvider, createGenerateClassName } from "@material-ui/core";
 
 import Landing from "./components/Landing";
@@ -9,15 +9,15 @@ const generateClassname = createGenerateClassName({
   productionPrefix: "marketing",
 });
 
-export default function App() {
+export default function App({ history }) {
   return (
     <StylesProvider generateClassName={generateClassname}>
-      <BrowserRouter>
+      <Router history={history}>
         <Switch>
           <Route exact path="/pricing" component={Pricing} />
           <Route path="/" component={Landing} />
         </Switch>
-      </BrowserRouter>
+      </Router>
     </StylesProvider>
   );
 }
